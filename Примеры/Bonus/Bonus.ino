27bd20a2df36f0f5a8ac1071438e403d23a594aa
voencev <95476667+voencev@users.noreply.github.com>
2025-01-11 12:20:00 +0300

    Update Bonus.ino

--- Примеры/Bonus/Bonus.ino
+++ Примеры/Bonus/Bonus.ino
@@ -18,42 +18,38 @@ void setup(void)
   testdrawtext();
 }
 
-String utf8rus(String source)
+#define maxString 100 // ограничиваем строку шириной экрана
+char target[maxString + 1] = "";
+
+char *utf8rus2(char *source)
 {
-  int i,k;
-  String target;
+  int i,j,k;
   unsigned char n;
   char m[2] = { '0', '\0' };
-
-  k = source.length(); i = 0;
-
+  strcpy(target, ""); k = strlen(source); i = j = 0;
   while (i < k) {
     n = source[i]; i++;
 
-    if (n >= 0xC0) {
+    if (n >= 127) {
       switch (n) {
-        case 0xD0: {
+        case 208: {
           n = source[i]; i++;
-          if (n == 0x81) { n = 0xA8; break; }
-          if (n >= 0x90 && n <= 0xBF) n = n + 0x30;
+          if (n == 129) { n = 192; break; } // перекодируем букву Ё
           break;
         }
-        case 0xD1: {
+        case 209: {
           n = source[i]; i++;
-          if (n == 0x91) { n = 0xB8; break; }
-          if (n >= 0x80 && n <= 0x8F) n = n + 0x70;
+          if (n == 145) { n = 193; break; } // перекодируем букву ё
           break;
         }
       }
     }
-    if (n >= 192 && n <= 223){n = n - 1;}
-    if (n >= 224 && n <= 255){n = n - 1;}
-    m[0] = n; target = target + String(m);
+
+    m[0] = n; strcat(target, m);
+    j++; if (j >= maxString) break;
   }
-return target;
+  return target;
 }
-
-
 
 void loop()
 {
@@ -61,7 +57,7 @@ void loop()
 
 
 
-void testdrawtext()
+void drawtext()
 {
   tft.setCursor(58,116);
   tft.setTextSize(3);
@@ -79,16 +75,13 @@ void testdrawtext()
   tft.setCursor(0, 16);
   tft.setTextSize(2);
   tft.setTextColor(ST77XX_WHITE);
-  tft.print(utf8rus("Елизавета Михайловна"));
+  tft.print(utf8rus2("Елизавета Михайловна"));
   tft.setCursor(110, 32);
-  tft.print(utf8rus("мы"));
-  tft.setCursor(105, 48);
-  tft.print(utf8rus("вас"));
+  tft.print(utf8rus2("мы"));
+  tft.setCursor(105, 50);
+  tft.print(utf8rus2("вас"));
   
   tft.setCursor(78, 80);
   tft.setTextSize(3);
-  tft.print(utf8rus("ЛЮБИМ"));
+  tft.print(utf8rus2("ЛЮБИМ"));
 }
-
-
-
